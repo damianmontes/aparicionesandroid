2fe2334f0838668197b7c15264e00c97bb68b7f5
esbva <esbxp-game@yahoo.es>
2020-01-05 19:10:33 -0300

    escenario.tscn, nuevo

---
+++ playerBody.gd
@@ -0,0 +1,33 @@
+extends KinematicBody
+
+var speedWalk = 200
+var angulo    = 0.0
+var avanzar   = Vector3()
+var direccion = Vector3()
+var velocidad = Vector3()
+
+
+func _physics_process(delta):
+	avanzar = Vector3(0,0,0)
+	
+	if(Input.is_key_pressed(KEY_W)):
+		avanzar = Vector3(0 ,0,  speedWalk)
+	
+	if(Input.is_key_pressed(KEY_S)):
+		avanzar = Vector3(0, 0, -speedWalk)
+	
+	if(Input.is_key_pressed(KEY_A)):
+		angulo += 2
+	if(Input.is_key_pressed(KEY_D)):
+		angulo -= 2
+		
+	rotation.y = deg2rad(angulo)
+	direccion = avanzar.rotated(Vector3(0,1,0), rotation.y)
+	velocidad = direccion * delta
+	
+	move_and_slide(velocidad, Vector3(0,1,0))
+	
+	
+	
+	
+	pass
