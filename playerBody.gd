97b21e7fd4ea337234b94fe61266232521c01a74
esbvaGames <esbva2002@gmail.com>
2020-01-06 11:38:51 -0300

    camara animacion

--- playerBody.gd
+++ playerBody.gd
@@ -5,15 +5,18 @@ var angulo    = 0.0
 var avanzar   = Vector3()
 var direccion = Vector3()
 var velocidad = Vector3()
+var accion  = ''
 
 
 func _physics_process(delta):
 	avanzar = Vector3(0,0,0)
-	
+	accion  = 'esperando'
 	if(Input.is_key_pressed(KEY_W)):
+		accion = 'caminar'
 		avanzar = Vector3(0 ,0,  speedWalk)
 	
 	if(Input.is_key_pressed(KEY_S)):
+		accion = 'caminar'
 		avanzar = Vector3(0, 0, -speedWalk)
 	
 	if(Input.is_key_pressed(KEY_A)):
@@ -26,8 +29,7 @@ func _physics_process(delta):
 	velocidad = direccion * delta
 	
 	move_and_slide(velocidad, Vector3(0,1,0))
-	
-	
-	
-	
+	if($animCamera.get_current_animation() != accion):
+		$animCamera.play(accion, 0.25)
+		#print('animacion: ', accion)
 	pass
